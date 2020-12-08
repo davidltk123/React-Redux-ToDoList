dd2e006854d2af5f94a8811ad16d978c63b89830
davidltk123 <davidltk123@gmail.com>
2020-12-08 19:41:13 +0800

    [todolist][david] feat: add deleteToDo action, handle it in reducer

--- src/action.js
+++ src/action.js
@@ -1,6 +1,11 @@
-import { ADD_TO_DO} from './actionType';
+import { ADD_TO_DO, DELETE_TO_DO} from './actionType';
 
 export const addToDo = (content) => ({
     type: ADD_TO_DO,
     payload: content
+})
+
+export const deleteToDo = (id) => ({
+    type: DELETE_TO_DO,
+    payload: id
 })
--- src/reducers.js
+++ src/reducers.js
@@ -1,11 +1,13 @@
-import { ADD_TO_DO } from './actionType'
+import { ADD_TO_DO, DELETE_TO_DO } from './actionType'
 import { v4 as uuidv4 } from 'uuid';
 import { combineReducers } from "redux";
 
 const toDoList = (state = [], action) => {
-    switch(action.type){
+    switch (action.type) {
         case ADD_TO_DO:
             return [...state, { id: uuidv4(), content: action.payload, complete: false }];
+        case DELETE_TO_DO:
+            return state.filter((todo) => todo.id !== action.payload);
         default:
             return state;
     }
