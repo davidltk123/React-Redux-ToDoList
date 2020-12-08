f6f609bc459716b41055eb8101d698c1a803a6c3
davidltk123 <davidltk123@gmail.com>
2020-12-08 20:21:07 +0800

    [todo list][david] feat: add ToDoList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-import ToDoGeneratorContainer from './containers/ToDoGeneratorContainer';
-import ToDoGroupContainer from './containers/ToDoGroupContainer';
+import ToDoList from './components/ToDoList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ToDoGroupContainer />
-        <ToDoGeneratorContainer />
+        <ToDoList/>
       </header>
     </div>
   );
---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ToDoGroupContainer from "../containers/ToDoGroupContainer";
+import ToDoGeneratorContainer from "../containers/ToDoGeneratorContainer";
+
+class ToDoList extends Component {
+    render() {
+        return (
+            <div>
+                <ToDoGroupContainer />
+                <ToDoGeneratorContainer />
+            </div>
+        );
+    }
+}
+
+export default ToDoList;
